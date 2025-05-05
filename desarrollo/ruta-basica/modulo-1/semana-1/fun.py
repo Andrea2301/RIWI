26716c8e66c7b220b9e9c9f8c69b90586ea8fe7c
Andrea2301 <123708674+Andrea2301@users.noreply.github.com>
2025-05-05 13:31:04 -0500

    Add files via upload

---
+++ desarrollo/ruta-basica/modulo-1/semana-1/fun.py
@@ -0,0 +1,11 @@
+def multiplica(a,b):
+    resultado = a * b
+    name= "esteban"
+    print(name)
+    print("1")
+    print("4")
+
+print("2")
+productValue = multiplica(2,8)
+print("3")
+print(productValue)
